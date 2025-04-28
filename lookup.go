7517292498226c231ac6e7e68df9646fa7272ae4
Victor de Moura Netto <victorhmnetto@gmail.com>
2025-04-28 13:56:21 +0200

    feat: obtain peers asked during the lookup

--- lookup.go
+++ lookup.go
@@ -34,12 +34,12 @@ func (dht *IpfsDHT) GetClosestPeers(ctx context.Context, key string) ([]peer.ID,
 		return nil, err
 	}
 
-	if err := ctx.Err(); err != nil || !lookupRes.completed {
-		return lookupRes.peers, err
+	if err := ctx.Err(); err != nil || !lookupRes.Completed {
+		return lookupRes.Peers, err
 	}
 
 	// tracking lookup results for network size estimator
-	if err = dht.nsEstimator.Track(key, lookupRes.closest); err != nil {
+	if err = dht.nsEstimator.Track(key, lookupRes.Closest); err != nil {
 		logger.Warnf("network size estimator track peers: %s", err)
 	}
 
@@ -50,7 +50,89 @@ func (dht *IpfsDHT) GetClosestPeers(ctx context.Context, key string) ([]peer.ID,
 	// refresh the cpl for this key as the query was successful
 	dht.routingTable.ResetCplRefreshedAtForID(kb.ConvertKey(key), time.Now())
 
-	return lookupRes.peers, nil
+	return lookupRes.Peers, nil
+}
+
+func (dht *IpfsDHT) GetPathClosestPeers(ctx context.Context, key string) ([]peer.ID, *qpeerset.QueryPeerset, error) {
+	ctx, span := internal.StartSpan(ctx, "IpfsDHT.GetClosestPeers", trace.WithAttributes(internal.KeyAsAttribute("Key", key)))
+	defer span.End()
+
+	if key == "" {
+		return nil, nil, fmt.Errorf("can't lookup empty key")
+	}
+
+	//TODO: I can break the interface! return []peer.ID
+	lookupRes, err := dht.runLookupWithFollowup(ctx, key, dht.pmGetClosestPeers(key), func(*qpeerset.QueryPeerset) bool { return false })
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if err := ctx.Err(); err != nil || !lookupRes.Completed {
+		return lookupRes.Peers, nil, err
+	}
+
+	// tracking lookup results for network size estimator
+	if err = dht.nsEstimator.Track(key, lookupRes.Closest); err != nil {
+		logger.Warnf("network size estimator track peers: %s", err)
+	}
+
+	if ns, err := dht.nsEstimator.NetworkSize(); err == nil {
+		metrics.NetworkSize.M(int64(ns))
+	}
+
+	// refresh the cpl for this key as the query was successful
+	dht.routingTable.ResetCplRefreshedAtForID(kb.ConvertKey(key), time.Now())
+
+	fmt.Println(peer.ID(key), lookupRes.Peers)
+	fmt.Printf("[ ")
+	for _, peer := range lookupRes.AllPeersContacted.GetClosestInStates(qpeerset.PeerHeard, qpeerset.PeerWaiting, qpeerset.PeerQueried) {
+		fmt.Printf("%s ,", peer)
+	}
+	fmt.Printf("]")
+
+	return lookupRes.Peers, lookupRes.AllPeersContacted, nil
+}
+
+func (dht *IpfsDHT) GetPathClosestPeersFullInformation(ctx context.Context, key string) (*LookupWithFollowupResult, error) {
+	ctx, span := internal.StartSpan(ctx, "IpfsDHT.GetClosestPeers", trace.WithAttributes(internal.KeyAsAttribute("Key", key)))
+	defer span.End()
+
+	if key == "" {
+		return nil, fmt.Errorf("can't lookup empty key")
+	}
+
+	//TODO: I can break the interface! return []peer.ID
+	lookupRes, err := dht.runLookupWithFollowup(ctx, key, dht.pmGetClosestPeers(key), func(*qpeerset.QueryPeerset) bool { return false })
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err := ctx.Err(); err != nil || !lookupRes.Completed {
+		return lookupRes, err
+	}
+
+	// tracking lookup results for network size estimator
+	if err = dht.nsEstimator.Track(key, lookupRes.Closest); err != nil {
+		logger.Warnf("network size estimator track peers: %s", err)
+	}
+
+	if ns, err := dht.nsEstimator.NetworkSize(); err == nil {
+		metrics.NetworkSize.M(int64(ns))
+	}
+
+	// refresh the cpl for this key as the query was successful
+	dht.routingTable.ResetCplRefreshedAtForID(kb.ConvertKey(key), time.Now())
+
+	// fmt.Println(peer.ID(key), lookupRes.Peers)
+	// fmt.Printf("[ ")
+	// for _, peer := range lookupRes.AllPeersContacted.GetClosestInStates(qpeerset.PeerHeard, qpeerset.PeerWaiting, qpeerset.PeerQueried) {
+	// 	fmt.Printf("%s ,", peer)
+	// }
+	// fmt.Printf("]")
+
+	return lookupRes, nil
 }
 
 // pmGetClosestPeers is the protocol messenger version of the GetClosestPeer queryFn.
@@ -62,7 +144,7 @@ func (dht *IpfsDHT) pmGetClosestPeers(key string) queryFn {
 			ID:   p,
 		})
 
-		peers, err := dht.protoMessenger.GetClosestPeers(ctx, p, peer.ID(key))
+		peers, err := dht.ProtoMessenger.GetClosestPeers(ctx, p, peer.ID(key))
 		if err != nil {
 			logger.Debugf("error getting closer peers: %s", err)
 			routing.PublishQueryEvent(ctx, &routing.QueryEvent{
